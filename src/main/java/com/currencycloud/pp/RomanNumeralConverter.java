c511c15a958e77d63afce4bc1eb7a7060fb5fbb6
Jonathan Head <jalhead@gmail.com>
2023-08-15 18:17:16 +0100

    a

--- src/main/java/com/currencycloud/pp/RomanNumeralConverter.java
+++ src/main/java/com/currencycloud/pp/RomanNumeralConverter.java
@@ -1,15 +1,60 @@
 package com.currencycloud.pp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Converter to convert to and from Roman numerals. 
  */
 public final class RomanNumeralConverter {
+  static Map<Character, Integer> charValues;
+  static {
+     charValues = new HashMap<>();
+     charValues.put('I',1);
+     charValues.put('V',5);
+     charValues.put('X',10);
+     charValues.put('L',50);
+     charValues.put('C',100);
+  }
+
   /**
    * Convert a Roman numeral into the integer equivalent.
    * @param value Value to convert.
    * @return      The integer value.
    */
   public static int convert(final String value) {
-    return 1;
+    //If the value of the symbol on the right is smaller, add it, otherwise if the value on the right is higher, subtract it
+    int result = 0;
+
+    if (value == null) {
+      return -1; //TODO
+    }
+    for (int ii = 0; ii< value.length(); ii++) {
+      char ch = value.charAt(ii);
+
+      Integer intValue;
+      if (charValues.containsKey(ch)) {
+        intValue = charValues.get(ch);
+      } else {
+        return -1; //TODO;
+      }
+      Integer intValueNext = -1;
+      char nextCh;
+      if (ii < (value.length() - 1)) {
+        nextCh = value.charAt(ii + 1);
+        if (charValues.containsKey(nextCh)) {
+          intValueNext = charValues.get(nextCh);
+        } else {
+          return -1; //TODO;
+        }
+      }
+      //determine if character to the right is of greater value than the current character (if so decrement, otherwise increment)
+      if ((intValueNext != -1) && (intValueNext > intValue)) {
+        result -= intValue;
+      } else {
+        result += intValue;
+      }
+    }
+    return result;
   }
 }
--- src/test/java/com/currencycloud/pp/RomanNumeralConverterTest.java
+++ src/test/java/com/currencycloud/pp/RomanNumeralConverterTest.java
@@ -8,8 +8,98 @@ import org.junit.Test;
  * Unit tests for {@link RomanNumeralConverter}.
  */
 public final class RomanNumeralConverterTest {
+
+  @Test
+  public void convert_blank_returns0() {
+    assertEquals(0, RomanNumeralConverter.convert(""));
+  }
+
   @Test
   public void convert_i_returns1() {
-    assertEquals(1, RomanNumeralConverter.convert("i"));
+    assertEquals(1, RomanNumeralConverter.convert("I"));
   }
+
+  @Test
+  public void convert_ii_returns2() {
+    assertEquals(2, RomanNumeralConverter.convert("II"));
+  }
+
+  @Test
+  public void convert_ii_returns3() {
+    assertEquals(3, RomanNumeralConverter.convert("III"));
+  }
+
+  @Test
+  public void convert_iv_returns4() {
+    assertEquals(4, RomanNumeralConverter.convert("IV"));
+  }
+
+  @Test
+  public void convert_v_returns5() {
+    assertEquals(5, RomanNumeralConverter.convert("V"));
+  }
+
+  @Test
+  public void convert_xiv_returns14() {
+    assertEquals(14, RomanNumeralConverter.convert("XIV"));
+  }
+
+  @Test
+  public void convert_xv_returns15() {
+    assertEquals(15, RomanNumeralConverter.convert("XV"));
+  }
+
+  @Test
+  public void convert_vi_returns6() {
+    assertEquals(6, RomanNumeralConverter.convert("VI"));
+  }
+
+  @Test
+  public void convert_vii_returns7() {
+    assertEquals(7, RomanNumeralConverter.convert("VII"));
+  }
+
+  @Test
+  public void convert_ix_returns9() {
+    assertEquals(9, RomanNumeralConverter.convert("IX"));
+  }
+
+  @Test
+  public void convert_xvii_returns17() {
+    assertEquals(17, RomanNumeralConverter.convert("XVII"));
+  }
+
+  @Test
+  public void convert_xxii_returns22() {
+    assertEquals(22, RomanNumeralConverter.convert("XXII"));
+  }
+
+  @Test
+  public void convert_xxiv_returns24() {
+    assertEquals(24, RomanNumeralConverter.convert("XXIV"));
+  }
+
+  @Test
+  public void convert_xlv_returns45() {
+    assertEquals(45, RomanNumeralConverter.convert("XLV"));
+  }
+
+  @Test
+  public void convert_xc_returns90() {
+    assertEquals(90, RomanNumeralConverter.convert("XC"));
+  }
+
+  @Test
+  public void convert_xcvii_returns97() {
+    assertEquals(97, RomanNumeralConverter.convert("XCVII"));
+  }
+  @Test
+  public void convert_xcix_returns99() {
+    assertEquals(99, RomanNumeralConverter.convert("XCIX"));
+  }
+  @Test
+  public void convert_xliv_returns44() {
+    assertEquals(44, RomanNumeralConverter.convert("XLIV"));
+  }
+
 }
